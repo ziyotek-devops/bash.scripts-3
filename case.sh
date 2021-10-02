d773faffd7bf7147e0c032b3ebedd73cb06119c4
rpaskalev <rpaskalev@lomixtech.com>
2021-10-01 21:49:51 -0400

    purposely broke shell script case

--- case.sh
+++ case.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+#When called, this script has the purpose of translating state initials into full state name.
+
 read "State initials " 
 
 case $REPLY in
@@ -12,9 +15,37 @@ echo "Virginia";;
 
 "AZ" )
 echo "Arizona";;
- * )
 
+"MD" )
+echo "Maryland";;
+
+"FL" )
+echo "Florida";;
+
+"TX" )
+echo "Texas";;
+
+"CA" )
+echo "California";;
+
+"DC" )
+echo "Washington D.C";;
+
+"OH" )
+echo "Ohio";;
+
+* )
 echo "enetr valid state initials"
 
 esac
+
+if [[ $REPLY =~ ^OH|NY|VA|AZ|MD|FL|TX|CA|DC ]]
+then ;
+echo "YEY! you fixed it"
+elif [[ $REPLY !=  @(def|OH|NY|VA|AZ|MD|FL|TX|CA|DC) ]] 
+then ;
+echo "FIX AS WELL!"
+else ;
+echo "Not fixed"
+fi
 
